32aafaff8b7d58058d25a9d65fb27e0a99f95615
Spyko <39496944+Spyko04@users.noreply.github.com>
2020-12-18 01:55:02 +0000

    Update bump.js

--- bump.js
+++ bump.js
@@ -6,7 +6,7 @@ if(message.guild.id === "ID de ton serveur"){
       if(message.embeds[0].description.includes(msgrequis)){
       // N'oublier pas de rajouter votre fonction si vous voulez donner des crédits à la personne qui bump votre serveur !
       // Exemple avec quick.db :
-      //db.add(votrefonction_${auteur.id}, 10) 
+      //db.add(votrefonction_${auteur}, 10) 
        message.channel.send(`<@${auteur}> vous avez gagné un truc !`)
       }else if(message.embeds[0].description.includes("avant que le serveur puisse être bumpé !")){
         message.channel.send(`<@${auteur}> vous n'avez rien gagné !`)
